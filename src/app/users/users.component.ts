cec73563cbb1babe30913954c154082006265cdd
Giancarlo Sparano <giancarlo.sparano@eng.it>
2019-11-11 18:46:03 +0100

    Rimossi warnig nel file src/app/users/users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
     selector : 'app-users',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core'
     styleUrls : ['./users.component.css']
 })
 
-export class UsersComponent{
+export class UsersComponent {
 }
